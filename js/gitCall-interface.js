b1c949c17ab97bd4462c45617868dd686853620a
Kyle Wolfson <wolfsonk@gmail.com>
2016-05-27 11:19:28 -0700

    added user profile info button, slightly different than optional specs, but hey, optional.

--- js/gitCall-interface.js
+++ js/gitCall-interface.js
@@ -1,17 +1,27 @@
 var apiKey = require('./../.env').apiKey;
 var gitCall = require('./../js/gitCall.js').gitCall;
+var profileCall = require('./../js/gitCall.js').profileCall;
+
+function displayProfile(owner) {
+  $('#apiReturn').append('<h1>Full name: ' + owner.name + '</h1>Email Address: '+ owner.email + '<br>Avatar: <br><img src=' + owner.avatar_url + '><br>Followers: ' + owner.followers);
+}
 
 function displayResult(repo) {
-  if (repo.description == ''){
+  if (repo.description === ''){
     repo.description = 'No description provided';
   }
-  $('#apiReturn').append('<strong>Repo name: ' + repo.name + '</strong><br>Repo description: ' + repo.description + '<br><br>');
+  $('#apiReturn').append('<strong>Repo name: ' + repo.name + '</strong><br>Repo description: ' + repo.description + '<br>Created on: ' + moment(repo.created_at).format('MMM Do YYYY') + '<br><br>');
 }
 
 $(document).ready(function() {
-  $('#makeCall').click(function() {
+  $('#requestRepos').click(function() {
     $('#apiReturn').empty();
     var userName = $('#userName').val();
     gitCall(userName, displayResult);
+  });
+  $('#requestProfile').click(function() {
+    $('#apiReturn').empty();
+    var userName = $('#userName').val();
+    profileCall(userName, displayProfile);
   });
 });
--- js/gitCall.js
+++ js/gitCall.js
@@ -1,5 +1,20 @@
 var apiKey = require('./../.env').apiKey;
 
+exports.profileCall = function(userName, displayProfile) {
+  var url;
+  if (apiKey) {
+    url = 'https://api.github.com/users/' + userName + '?access_token=' + apiKey;
+  } else {
+    url = 'https://api.github.com/users/' + userName;
+  }
+
+  $.ajax(url).then(function(response){
+    displayProfile(response);
+  }).fail(function(error){
+    console.log(error);
+  });
+};
+
 exports.gitCall = function(userName, displayResult) {
   var url;
   if (apiKey) {
@@ -8,8 +23,7 @@ exports.gitCall = function(userName, displayResult) {
     url = 'https://api.github.com/users/' + userName + '/repos';
   }
 
-  $.ajax(url).then(function(response)
-  {
+  $.ajax(url).then(function(response){
     response.forEach(function(repo) {
       displayResult(repo);
     });
