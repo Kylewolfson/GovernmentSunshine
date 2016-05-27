060098928d2db416265a6069dd70414b3634a0fd
Kyle Wolfson <wolfsonk@gmail.com>
2016-05-27 09:40:49 -0700

    got output working for name and description along with conditional for blank descriptions. Since someone might be really lazy about adding descriptions to their repos.

--- js/gitCall.js
+++ js/gitCall.js
@@ -3,6 +3,12 @@ var apiKey = require('./../.env').apiKey;
 exports.gitCall = function(userName) {
   $.ajax('https://api.github.com/users/' + userName + '/repos?access_token=' + apiKey).then(function(response)
   {
-    $('#apiReturn').text(response);
+    console.log(response);
+    response.forEach(function(repo) {
+      if (repo.description == ''){
+        repo.description = 'No description provided';
+      }
+      $('#apiReturn').append('<strong>Repo name: ' + repo.name + '</strong><br>Repo description: ' + repo.description + '<br><br>');
+    });
   });
 };
